e25b2930a20f79268e6b3d8acce93563a0004751
fadln11 <adeline.faustina00@gmail.com>
2019-01-31 22:57:09 -0600

    add magnific popup to stylesofpizza.html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,19 @@
+$(document).ready(function () {
 
     $("#header").load("header.html");
     $("#footer").load("footer.html");
+
+
+    // Popup Image
+    $('.image-popup-vertical-fit').magnificPopup({
+        type: 'image',
+        closeOnContentClick: true,
+        mainClass: 'mfp-img-mobile',
+        image: {
+            verticalFit: true
+        }
+    });
+
+
+
+});
